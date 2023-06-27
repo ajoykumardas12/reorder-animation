22e02916772a1c675e9bd87e211d6e9817ccb0d4
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-06-27 23:58:10 +0530

    don't trigger reorder if currently reordering

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import "./CardsContainer.css";
 
 export default function CardsContainer() {
+  // if reordering, don't trigger another reorder
+  const [isReordering, setIsReordering] = useState(false);
+
   function reorder() {
     const cardsContainer = document.getElementById("cards");
     const cards = cardsContainer?.children;
-    if (cards) {
+
+    if (cards && !isReordering) {
       const tempCard = cards[0];
       cards[0].classList.add("reorder-animation");
+      setIsReordering(true);
 
-      //
       setTimeout(() => {
         cardsContainer.removeChild(cards[0]);
         tempCard.classList.remove("reorder-animation");
         cardsContainer.appendChild(tempCard);
+        setIsReordering(false);
       }, 1000);
     }
   }
@@ -30,7 +36,7 @@ export default function CardsContainer() {
           Card 3
         </div>
       </div>
-      <button className="" onClick={reorder}>
+      <button className="" onClick={reorder} disabled={isReordering}>
         Reorder
       </button>
     </div>
